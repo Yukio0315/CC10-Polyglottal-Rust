e2c0e5d1536584902ecf4c7cf90e18d324255d17
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2019-11-29 14:57:23 +0900

    add test for selection_sort

--- src/selection_sort.rs
+++ src/selection_sort.rs
@@ -16,3 +16,16 @@ pub fn sort(array: &mut [i32]) {
         i += 1;
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn sort_i32_array() {
+        let mut x = [10, 9, 3, 2, 5, 0, 1];
+
+        sort(&mut x);
+
+        assert_eq!(x, [0, 1, 2, 3, 5, 9, 10]);
+    }
+}
